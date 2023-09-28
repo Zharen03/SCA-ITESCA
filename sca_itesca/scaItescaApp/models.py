8a765dd6a9b9773febe0691fdbf33e541ccece63
MarioS <mariosandoval903@gmail.com>
2023-09-27 22:22:47 -0700

    feature: Area and User models

--- sca_itesca/scaItescaApp/models.py
+++ sca_itesca/scaItescaApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-#a
+class Area(models.Model):
+    name = models.CharField(max_length=255)
+
+class User(models.Model):
+    payroll_number = models.PositiveBigIntegerField(primary_key=True, null=False, unique=True)
+    password = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone_number = models.PositiveIntegerField()
+    min_gender = models.BooleanField()
+    min_general = models.BooleanField()
+    role = models.IntegerField()
+    status = models.BooleanField()
+    area_id = models.ForeignKey(Area, on_delete=models.CASCADE)
+    
+    
