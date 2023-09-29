7bf2f548d375dc97e3b3faa9aa8fe98457eb5dd8
rapavane <rapavane1998@gmail.com>
2023-09-28 19:16:05 -0700

    feature: added models

--- sca_itesca/scaItescaApp/models.py
+++ sca_itesca/scaItescaApp/models.py
@@ -16,5 +16,65 @@ class User(models.Model):
     role = models.IntegerField()
     status = models.BooleanField()
     area_id = models.ForeignKey(Area, on_delete=models.CASCADE)
+
+class Training(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    attendance_code = models.CharField(max_length=5)
+    modality = models.BooleanField()
+    general = models.BooleanField()
+    internal = models.BooleanField()
+    status = models.BooleanField()
+
+class Invitation(models.Model):
+    description = models.CharField(max_length=255)
+    optional = models.BooleanField()
+    status = models.BooleanField()
+    training_id = models.ForeignKey(Training, on_delete=models.CASCADE)
+
+class Date(models.Model):
+    day = models.DateField()
+
+class Question(models.Model):
+    description = models.CharField(max_length=255)
+
+class User_Invitation(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    invitation_id = models.ForeignKey(Invitation, on_delete=models.CASCADE)
+
+class User_Training(models.Model):
+    training_id = models.ForeignKey(Training, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    attendance = models.BooleanField()
+    certified = models.BooleanField()
+
+class Certificate(models.Model):
+    training_id = models.ForeignKey(Training, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=255)
+    external = models.BooleanField()
+    status = models.BooleanField()
+
+class Evaluation(models.Model):
+    type = models.IntegerField()
+    training_id = models.ForeignKey(Training, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Date_Training(models.Model):
+    training_id = models.ForeignKey(Training, on_delete=models.CASCADE)
+    date_id = models.ForeignKey(Date, on_delete=models.CASCADE)
+    time = models.TimeField()
+
+class Evaluation_Question(models.Model):
+    evaluation_id = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255)
+
+class DNC(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=65535)
+    status = models.BooleanField()
+
     
     
