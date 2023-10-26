eb474c16376b5ab5d393e49859e931222893102e
LizethJackelineVazquez <lizeth.vazquez.001.07@gmail.com>
2023-10-26 02:46:30 -0700

    feature: create wokrplan

--- sca_itesca/scaItescaApp/urls.py
+++ sca_itesca/scaItescaApp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('sca/virtual_training', views.virtualTrainingEventEvaluationForm, name='virtual_training'),
     path('sca/show_invitations', views.showInvitations, name='show_invitations'),
     path('sca/training_needs_request', views.trainingNeedsRequestForm, name='training_needs_request'),
+    path('sca/create_workplan', views.createWorkplan, name='create_workplan'),
 ]
