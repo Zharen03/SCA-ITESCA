ce19a31f2788dbdbd86ad309ee3b11ac0ebc690d
MarioS <mariosandoval903@gmail.com>
2023-11-21 20:18:59 -0700

    feature: login and user model fixed

--- sca_itesca/scaItescaApp/urls.py
+++ sca_itesca/scaItescaApp/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sca/login', views.login, name="login"),
+    path('sca/add_group', views.addGroup),
+    path('sca/login', views.loginView, name="login"),
+    path('sca/login_auth', views.loginAuth),
+    path('sca/logout_user', views.logoutUser),
+    
     path('sca/add_user_form', views.addUserForm, name='add_user_form'),
     path('sca/add_user', views.addUser, name="add_user"),
     path('sca/show_user', views.showUser, name='show_user'),
-    path('sca/show_dnc', views.showDNC, name='show_dnc'),
+    path("sca/add_area", views.addArea, name="add_area"),
     
+    path('sca/show_dnc', views.showDNC, name='show_dnc'),
     
     path('sca/resume_training_events', views.trainingEventEvaluationSummaryForm, name='resume_training_events'),
     path('sca/training_events_evaluation_form', views.trainingEventEvaluationForm, name='training_events'),
@@ -22,9 +27,9 @@ urlpatterns = [
     path('sca/training_plan', views.trainingPlan, name="training_plan"),
     path('sca/show_summary', views.showSummary, name="show_summary"),
     path('sca/capacitation_needs_detection', views.capacitationNeedsDetectionForm, name="capacitation_needs_detection"),
-    path('sca/add_dnc', views.addDNC, name='add_dnc')
+    path('sca/add_dnc', views.addDNC, name='add_dnc'),
     path('sca/training_needs_request_form', views.trainingNeedsRequestForm, name='training_needs_request'),
-    path('sca/training_needs_request', views.trainingNeedsRequest, name='training_needs')
+    path('sca/training_needs_request', views.trainingNeedsRequest, name='training_needs'),
     path('sca/create_workplan', views.createWorkplan, name='create_workplan'),
     path('sca/evaluations', views.evaluations, name="evaluations"),
     path('sca/certificates', views.certificatesNoAdmin, name="certificates_no_admin"),
--- sca_itesca/sca_itesca/settings.py
+++ sca_itesca/sca_itesca/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Customized user
+AUTH_USER_MODEL = "scaItescaApp.User"
+
+#Login view
+LOGIN_URL = '/sca/login'
+
+#Set to logout when browser is closed
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+#Set to logout when enough time has passed
+SESSION_COOKIE_AGE = 600
